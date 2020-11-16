a99e6533ec2603d982407ec464d9743904bd0df0
sochief <shafkatoo@gmail.com>
2020-11-16 13:29:51 +0300

    Local registration

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <h2 v-colored:background.font="'red'">{{ title }}</h2>
     <h2 v-colored:color.delay="'blue'">{{ title }}</h2>
+    <h2 v-font>Local font directive</h2>
   </div>
 </template>
 
@@ -13,8 +14,18 @@ export default {
       title: "Hello, Im Vue!",
     };
   },
+
+  /* eslint-disable */
+  directives: {
+    font: {
+      bind(el, bindings, vnode) {
+        el.style.fontSize = "40px";
+      },
+    },
+  },
 };
 </script>
+  /*eslint-enable*/
 
 
 <style scoped>
