5a1c011b1dbb9bf9d58a1fb6ebaf161caebbb83c
sochief <shafkatoo@gmail.com>
2020-11-16 11:21:37 +0300

    Directive Life cycle

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h2 v-colored>{{ title }}</h2>
+    <h2 v-colored v-if="visible">{{ title }}</h2>
+    <button @click="visible = !visible">Toggle</button>
+    <button @click="title = 'New title'">Change title</button>
   </div>
 </template>
 
@@ -10,6 +12,7 @@ export default {
   data() {
     return {
       title: "Hello, Im Vue!",
+      visible: true,
     };
   },
 };
--- src/color.js
+++ src/color.js
@@ -2,5 +2,17 @@ export default{
     bind(el,bindings,vnode){
         //when it initialises
         el.style.color = 'red'
+    },
+    inserted(el,bindings,vnode){
+        console.log('inserted')
+    },
+    update(el,bindings,vnode,oldVnode){
+        console.log('update')
+    },
+    componentUpdated(el,bindings,vnode,oldVnode){
+        console.log('component updated')
+    },
+    unbind(){
+        console.log('unbined')
     }
 }
