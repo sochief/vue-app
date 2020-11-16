58fbc007f116b274521b642198f1f9d8a42949dc
sochief <shafkatoo@gmail.com>
2020-11-16 13:25:05 +0300

    Modifiers

--- src/color.js
+++ src/color.js
@@ -1,9 +1,20 @@
 /* eslint-disable */
 export default{
     bind(el,bindings,vnode){
-        const arg = bindings.arg
-        // el.style.color = 'blue'
-        el.style[arg] = bindings.value
+        const fontModifier = bindings.modifiers['font']
+        if(fontModifier){
+            el.style.fontSize = '30px'
+        }
+        const delayModifier = bindings.modifiers['delay']
+
+        let delay = 0
+        if(delayModifier){
+            delay = 2000
+        }
+        setTimeout(() => {
+            const arg = bindings.arg
+            el.style[arg] = bindings.value
+        },delay)
     }
 }
 /* eslint-enable */
